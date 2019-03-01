067e3cc36a4cd197122287cf9b0a6d450960f9c3
董瑞祺 <drq@drq-mini.local>
2019-03-01 22:52:35 +0800

    init.

---
+++ app.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+var fileName = '/files.json';
+
+fs.readFile(__dirname + fileName, (err, buff) => {
+    if (err) throw err;
+    let filesArr = JSON.parse(buff.toString('utf8'));
+
+    
+});
