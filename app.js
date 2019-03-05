88b6a28ee59b9f430845aca86faed1c98aba2668
董瑞祺 <drq@drq-book.local>
2019-03-05 22:06:43 +0800

    增加 JSDOC；克隆[未完成]。

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
-const path = require('path');
 const cp = require('child_process');
+const path = require('path');
+const url = require('url')
 
+function git_clone(goPath, importPath, srcPath) {
+    let git = cp.spawn('git', ['clone', ])
+}
+
+/**
+ * 执行 Git 拉取命令。
+ * @param {String} goPath GOPATH 路径。
+ * @param {String} importPath import 指令路径。
+ */
 function git_pull(goPath, importPath) {
-    var git = cp.spawnSync('git', ['pull'], {
+    let git = cp.spawnSync('git', ['pull'], {
         // cwd: goPath + 'src/' + importPath
         cwd: path.join(goPath, 'src', importPath),
         stdio: 'inherit'
@@ -44,7 +54,12 @@ fs.readFile(__dirname + fileName, (err, buff) => {
 
     go_lib.forEach((v, i) => {
         console.log(i + 1 + '. ' + v['import']);
+        
         git_pull(go_path, v['import']);
+
+        if (v['cmd']) {
+            console.log('编译');
+        }
     });
 
 });
